fe14dedd12c5f67445d43791fdbe63639093450d
zaibacontractor <zaiba.matiwala@gmail.com>
2023-07-17 16:28:04 +0530

    initial version

---
+++ src/com/gurukul/day1/Main.java
@@ -0,0 +1,14 @@
+package com.gurukul.day1;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Zaiba");
+
+	}
+
+}
